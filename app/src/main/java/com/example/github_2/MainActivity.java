abe19e4333aa53c88082009e00488722d219b9b6
o0o1677 <o0o1677@naver.com>
2022-03-14 22:16:22 +0900

    연습 5번째

---
+++ app/src/main/java/com/example/github_2/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.github_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // 연동 테스트
+    }
+}
